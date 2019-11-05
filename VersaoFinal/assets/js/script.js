edf993dda44913e19313ea4a8dd494428a5f757c
Matheus Eduardo Barboza Santos <38351837+matheuseduardo09@users.noreply.github.com>
2019-11-04 23:32:52 -0300

    Finalizando e criando as 2 versoes do projeto

--- VersaoFinal/assets/js/script.js
+++ VersaoFinal/assets/js/script.js
@@ -44,9 +44,28 @@ var filterPokemon = function filterPokemon(name){
 
 var displayPokemon = function displayPokemon(pokemon) {
   var pokemonHTMLString = pokemon.map(function (pokeman) {
-    return "\n        <li class=\"card\">\n            <img class=\"card-image\" src=\"".concat(pokeman.image, "\"/>\n            <h2 class=\"card-title\">").concat(pokeman.name, "</h2>\n            <p class=\"card-subtitle\">Type: ").concat(pokeman.type, "</p>\n        </li>\n    ");
+    return "\n        <li class=\"card\">\n            <img class=\"card-image\" src=\"".concat(pokeman.image, "\"/>\n            <h2 class=\"card-title\">").concat(pokeman.name, "</h2>\n            <p class=\"card-subtitle\">Tipo: ").concat(pokeman.type, "</p>\n        </li>\n    ");
   }).join('');
   pokedex.innerHTML = pokemonHTMLString;
 };
+
+var alterarTituloNavegador = function alterarTituloNavegador(){
+
+  var frases = [
+    'Você tem o poder de construir seu próprio caminho',
+    'Por mais diferentes que sejamos, todos temos mais em comum do que imaginamos!',
+    'A amizade e trabalho de equipe está acima de tudo!',
+    'Pokémon e romance: o amor conta mais que tudo!',
+    'Sempre que necessário, coloque limites (ainda mais se você for uma mulher)',
+    'As lições de Pokémon: entenda que há valor e também riscos na ingenuidade'
+  ];
+  
+  setInterval(function(){
+    document.title = frases[Math.floor(Math.random() * 5)]
+  }, 3000);
+
+}
+
+alterarTituloNavegador();
 
 fetchPokemon();
